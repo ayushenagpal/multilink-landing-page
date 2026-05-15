dbadea5b937e63f9c4f870454bc4e5e73d7e7924
ayushenagpal <ayushenagpal@gmail.com>
2026-05-14 18:51:51 -0700

    Match legacy html URLs in redirect middleware

--- middleware.ts
+++ middleware.ts
@@ -29,5 +29,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|.*\\..*).*)"]
+  matcher: ["/((?!_next/static|_next/image).*)"]
 };
